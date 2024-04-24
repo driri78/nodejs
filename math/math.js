acbc549caecce68b574aee5fc33b7a383d21b465
driri <ksgkdw78@naver.com>
2024-04-24 20:07:24 +0900

    module.exports

---
+++ math/math.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+const sub = (a, b) => {
+  return a - b;
+};
+const mul = (a, b) => {
+  return a * b;
+};
+const divd = (a, b) => {
+  return a / b;
+};
+
+// 함수 꺼내기
+module.exports = { add, sub, mul, divd };
