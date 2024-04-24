b33dc4404b2cd8ff092285fe829649fe890b745d
driri <ksgkdw78@naver.com>
2024-04-24 21:37:06 +0900

    fs module

--- test09.js
+++ test09.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-// path :
-const path = require("path");
 
 // 코드를 입력할땐 확장자를 생략하면 안됨
 // utf-8
@@ -9,9 +7,18 @@ const path = require("path");
 // });
 // 문자열
 fs.readFile("./math/math.js", (err, data) => {
-  console.log(data.toString());
+  // console.log(data.toString());
 });
 
-fs.readFile("./data/lorem.txt", (err, data) => {
-  console.log(data.toString());
+fs.readFile("./data/lorem.txt", "utf-8", (err, data) => {
+  // console.log(data);
+  process.emit("data");
+});
+
+fs.readFile("./data/user.json", "utf-8", (err, data) => {
+  // console.log(data);
+});
+
+process.on("data", () => {
+  console.log("data event 발생");
 });
